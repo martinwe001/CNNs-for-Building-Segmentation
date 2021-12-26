ffce01847e22bc2a66537ea0cb0041b82374a5ec
Martin.w-e <43035215+martinwe001@users.noreply.github.com>
2021-12-26 19:58:40 +0100

    Update new_model.py

--- new_model.py
+++ new_model.py
@@ -32,12 +32,27 @@ def build_simpler_model(input_shape):
 
     b1 = conv_block(p3, 512)
 
-    d1 = decoder_block(b1, s3, 512)
+    d1 = decoder_block(b1, s3, 256)
     d2 = decoder_block(d1, s2, 128)
     d3 = decoder_block(d2, s1, 64)
 
     outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d3)
-    model = Model(inputs, outputs, name="U-Net")
+    model = Model(inputs, outputs, name="Simple-model")
+    return model
+
+def build_even_simpler_model(input_shape):
+    inputs = Input(input_shape)
+
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+
+    b1 = conv_block(p2, 256)
+    
+    d1 = decoder_block(b1, s2, 128)
+    d2 = decoder_block(d1, s1, 64)
+
+    outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d2)
+    model = Model(inputs, outputs, name="Even-Simpler-model")
     return model
 
 if __name__ == "__main__":
