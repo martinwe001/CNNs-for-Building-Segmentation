9209afe4a7ede822e3495080e704913887e34df3
Martin <martinwangeneriksen@gmail.com>
2021-12-26 19:00:17 +0100

    add simpler model

---
+++ new_model.py
@@ -0,0 +1,46 @@
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
+from tensorflow.keras.models import Model
+
+def conv_block(input, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(input)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    x = Conv2D(num_filters, 3, padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+
+    return x
+
+def encoder_block(input, num_filters):
+    x = conv_block(input, num_filters)
+    p = MaxPool2D((2, 2))(x)
+    return x, p
+
+def decoder_block(input, skip_features, num_filters):
+    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+    x = Concatenate()([x, skip_features])
+    x = conv_block(x, num_filters)
+    return x
+
+def build_model(input_shape):
+    inputs = Input(input_shape)
+
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+
+    b1 = conv_block(p3, 512)
+
+    d1 = decoder_block(b1, s3, 512)
+    d2 = decoder_block(d1, s2, 128)
+    d3 = decoder_block(d2, s1, 64)
+
+    outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d3)
+    model = Model(inputs, outputs, name="U-Net")
+    return model
+
+if __name__ == "__main__":
+    input_shape = (64, 64, 3)
+    model = build_model(input_shape)
+    model.summary()
